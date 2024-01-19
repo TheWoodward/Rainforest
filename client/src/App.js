8cea51030d2ef0e1ffe8ea460401a9e32474b43f
Edward Wood <2194801w@student.gla.ac.uk>
2024-01-19 16:46:01 +0000

    added online detection

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [connected, setConnected] = useState(false);
+
+  const checkConnection = () => {
+    axios
+      .get("http://localhost:8080/", {})
+      .then((res) => {
+        console.log(res.status);
+        if (res.status === 200) {
+          setConnected(true);
+        } else {
+          setConnected(false);
+        }
+      })
+      .catch((err) => {
+        setConnected(false);
+        console.error(err);
+      });
+  };
+
+  useEffect(() => {
+    checkConnection();
+    var intervalId = window.setInterval(function () {
+      checkConnection();
+    }, 5000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <button onClick={() => alert("hello world")}>Hello</button>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Are we connected?
+          <br></br>
+          {connected ? "yes" : "no"}
         </p>
         <a
           className="App-link"
